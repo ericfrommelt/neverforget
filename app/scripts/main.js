3489acee7adf6534f3e4cf9ba7ba26ea726c5c04
ericfrommelt <ericfrommelt@gmail.com>
2014-11-07 10:03:19 -0500

    refactored to update time properly

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,25 +6,19 @@ function runClock() {
   var seconds = time.getSeconds();
 
   // Hours
-  var hourNode = document.createElement('p');
-  var newHour = document.createTextNode(hour);
-  hourNode.appendChild(newHour);
-  var placeHour = document.getElementById('hours');
-  placeHour.appendChild(hourNode);
+  var hoursNode = document.getElementById('hours');
+  var theHour = hoursNode.innerHTML;
+  hoursNode.innerHTML = hour;
 
   // Minutes
-  var minutesNode = document.createElement('p');
-  var newMinute = document.createTextNode(minutes);
-  minutesNode.appendChild(newMinute);
-  var placeMinute = document.getElementById('minutes');
-  placeMinute.appendChild(minutesNode);
+  var minutesNode = document.getElementById('minutes');
+  var theMinute = minutesNode.innerHTML;
+  minutesNode.innerHTML = minutes;
 
   // Seconds
-  var secondsNode = document.createElement('p');
-  var newSecond = document.createTextNode(seconds);
-  secondsNode.appendChild(newSecond);
-  var placeSeconds = document.getElementById('seconds');
-  placeSeconds.appendChild(secondsNode);
+  var secondsNode = document.getElementById('seconds');
+  var theSeconds = secondsNode.innerHTML;
+  secondsNode.innerHTML = seconds;
 
   setTimeout(runClock, 1000);
 
