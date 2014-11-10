2a86c9f3ac947e06556fa49b56dbac1eeb105858
ericfrommelt <ericfrommelt@gmail.com>
2014-11-10 11:12:27 -0500

    added minute bar functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,23 @@
+// Get Initial Time
 var getCurrentTime = new Date();
+
+// Set initial seconds
 var getCurrentSecond = getCurrentTime.getSeconds();
 var setInitialSeconds = document.getElementById('seconds-wrapper');
 var setInitialSecondBars = setInitialSeconds.innerHTML;
-for (var j=1; j<=getCurrentSecond; j++) {
+for (var i=1; i<=getCurrentSecond; i++) {
   var newInitialSecondBar = document.createElement('div');
   setInitialSeconds.appendChild(newInitialSecondBar);
+}
+
+// Set initial minutes
+var getCurrentMinute = getCurrentTime.getMinutes();
+getCurrentMinute = getCurrentMinute + 1;
+var setInitialMinutes = document.getElementById('minutes-wrapper');
+var setInitialMinuteBars = setInitialMinutes.innerHTML;
+for (var i=1; i<getCurrentMinute; i++) {
+  var newInitialMinuteBar = document.createElement('div');
+  setInitialMinutes.appendChild(newInitialMinuteBar);
 }
 
 function runClock() {
@@ -31,13 +44,11 @@ function runClock() {
   secondsNode.innerHTML = seconds;
 
   // Generate bars for seconds
-
   var placeSecondBar = document.getElementById('seconds-wrapper');
   var secondBars = placeSecondBar.innerHTML;
   var newSecondBar = document.createElement('div');
 
   // If seconds equal 0 clear all bars
-
   if (seconds == 1) {
     placeSecondBar.innerHTML = '<div></div>';
   } else {
@@ -46,9 +57,16 @@ function runClock() {
     }
   }
 
+  // Generate bars for minutes
+  var placeMinuteBar = document.getElementById('minutes-wrapper');
+  var minuteBars = placeMinuteBar.innerHTML;
+  var newMinuteBar = document.createElement('div');
+
+  if (seconds == 1) {
+    placeMinuteBar.appendChild(newMinuteBar);
+  }
 
   setTimeout(runClock, 1000);
-
 }
 
 runClock();
